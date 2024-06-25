752395d757492a4f1ce4803af12403d9626e49a2
Xenthio <thegooster88@gmail.com>
2024-06-25 16:52:02 +1000

    Create shared.lua

---
+++ lua/autorun/shared.lua
@@ -0,0 +1,18 @@
+if (SERVER) then
+    hook.Add( "TTT2PlayerReady", "SourceTVFix", function( ply )
+        if ply:Nick() == "SourceTV" then
+            print("[Source TV TTT] - Forcing SourceTV into spectator")  
+            --if not ply:IsSpec() then
+            --    ply:Kill()
+            --end
+
+            GAMEMODE:PlayerSpawnAsSpectator(ply)
+
+            ply:SetTeam(TEAM_SPEC)
+            ply:SetForceSpec(true)
+            ply:Spawn()
+
+            ply:SetRagdollSpec(false) -- dying will enable this, we don't want it here
+        end
+    end )
+end
