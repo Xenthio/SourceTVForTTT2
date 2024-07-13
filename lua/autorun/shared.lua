d754e8ad0bc461b84b9cf231d3461d87bf3a1415
Xenthio <thegooster88@gmail.com>
2024-07-13 15:22:32 +1000

    Lets get this shit workin

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -1,6 +1,7 @@
 if (SERVER) then
-    hook.Add( "TTT2PlayerReady", "SourceTVFix", function( ply )
-        if ply:Nick() == "SourceTV" then
+    function TryFixup(ply)
+        print("[Source TV TTT] [Debug] - Attempting to fix " .. ply:Nick() .. ". (checking for " .. GetConVar("tv_name"):GetString() .. ")")  
+        if ply:Nick() == GetConVar("tv_name"):GetString() then
             print("[Source TV TTT] - Forcing SourceTV into spectator")  
             --if not ply:IsSpec() then
             --    ply:Kill()
@@ -14,5 +15,14 @@ if (SERVER) then
 
             ply:SetRagdollSpec(false) -- dying will enable this, we don't want it here
         end
+    end
+    hook.Add( "TTTBeginRound", "SourceTVFix", function( ply )
+        for k,v in pairs(player.GetAll()) do
+            TryFixup(v)
+        end
     end )
+    --for k,v in pairs(player.GetAll()) do
+    --    TryFixup(v)
+    --end
+    print("[Source TV TTT] - Source TV fixes is active and initialised!")
 end
